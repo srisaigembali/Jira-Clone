53a5e9cca911560fbbf2e9f66dd2fa7c5c601131
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-30 12:36:32 +0530

    added user authentication using clerk

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,21 +14,39 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
-			<body className={`${inter.className}`}>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='dark'
-				>
-					<Header />
-					<main className='min-h-screen'>{children}</main>
-					<footer className='bg-gray-900 py-12'>
-						<div className='container mx-auto px-4 text-center text-gray-200'>
-							<p>Made with &#x1F496; by Srisai</p>
-						</div>
-					</footer>
-				</ThemeProvider>
-			</body>
-		</html>
+		<ClerkProvider
+			appearance={{
+				baseTheme: shadesOfPurple,
+				variables: {
+					colorPrimary: "#3b82f6",
+					colorBackground: "#1a202c",
+					colorInputBackground: "#2D3748",
+					colorInputText: "#F3F4F6",
+				},
+				elements: {
+					formButtonPrimary: "text-white",
+					card: "bg-gray-800",
+					headerTitle: "text-blue-400",
+					headerSubtitle: "text-gray-400",
+				},
+			}}
+		>
+			<html lang='en'>
+				<body className={`${inter.className}`}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='dark'
+					>
+						<Header />
+						<main className='min-h-screen'>{children}</main>
+						<footer className='bg-gray-900 py-12'>
+							<div className='container mx-auto px-4 text-center text-gray-200'>
+								<p>Made with &#x1F496; by Srisai</p>
+							</div>
+						</footer>
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</div>
+	);
 };
 
 export default Header;
